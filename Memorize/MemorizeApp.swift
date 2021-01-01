7f8a90432b85b17e757b8f3ed201be5d91ecc637
Gleydson Rodrigues <gleydsonsr@gmail.com>
2021-01-01 19:17:16 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Gleydson Rodrigues on 01/01/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
