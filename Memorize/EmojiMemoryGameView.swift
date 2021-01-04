e3fcacfbf5d1f932386a334c2c9c9503ce80937f
Gleydson Rodrigues <gleydsonsr@gmail.com>
2021-01-03 21:00:17 -0300

    Create pie shape

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -32,6 +32,9 @@ struct CardView: View {
                 if card.isFaceUp {
                     RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
                     RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
+                    Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(110-90), clockwise: true)
+                        .padding(5)
+                        .opacity(0.4)
                     Text(card.content)
                 } else {
                     if !card.isMatched {
@@ -48,14 +51,14 @@ struct CardView: View {
     private let cornerRadius: CGFloat = 10.0
     private let edgeLineWidth: CGFloat = 3.0
     private func fontSize(for size: CGSize) -> CGFloat {
-        min(size.width, size.height) * 0.75
+        min(size.width, size.height) * 0.7
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        Group {
-            EmojiMemoryGameView(game: EmojiMemoryGame())
-        }
+        let game = EmojiMemoryGame()
+        game.choose(card: game.cards[0])
+        return EmojiMemoryGameView(game: game)
     }
 }
